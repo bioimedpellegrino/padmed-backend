f3e4a0d5e1025ffa08ca7f07a09f0d258cf3e02b
Alessandro Pellegrino <alessandro.pellegrino@prodigysgroup.it>
2021-09-25 15:52:24 +0200

    dev: db

--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME'  : 'db.sqlite3',
+        'NAME'  : os.path.join(CORE_DIR, 'db.sqlite3'),
     }
 }
 
