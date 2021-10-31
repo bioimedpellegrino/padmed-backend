d45ad3a3f26db164bf6b48f1173cacf4b363aca9
Ale Pelle <alessandro.pellegrino9494@gmail.com>
2021-10-31 10:37:06 +0100

    dev: development, .

--- triage/urls.py
+++ triage/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from django.conf.urls import url
 from .views import *
 
+#triage/
 urlpatterns=[
+    # Triage
     path('decodefiscalcode/', DecodeFiscalCodeView.as_view(), name='decode_fiscal_code'),
-    path('testdfxapi/', TestDFXApiView.as_view(), name='test_dfx_api'),
     path('receptions/', ReceptionsView.as_view(), name='receptions'),
     path('receptions_accessreason/<int:access_id>/<int:reason_id>/', ReceptionsReasonsView.as_view(), name='receptions_accessreason'),
     path('recordvideo/<int:access_id>/', RecordVideoView.as_view(), name='record_video'),
-    path('patientresults/', PatientResults.as_view(), name='patient_results')
+    path('patientresults/', PatientResults.as_view(), name='patient_results'),
+    
+    # DFXAPI
+    path('testdfxapi/', TestDFXApiView.as_view(), name='test_dfx_api'),
+
 ]
