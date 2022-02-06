ae0534e5a6acb51845823ce9888a46aac6cbf998
Cristian Carnevale <cristian.carnevale@prodigysgroup.it>
2022-02-06 12:23:53 +0100

    dev UI: rotations

--- triage/utils.py
+++ triage/utils.py
@@ -38,7 +38,9 @@ def unpack_result_deepaffex(deep_affex_result):
     result_unpacked["ID"] = deep_affex_result["ID"]
     result_unpacked["StatusID"] = deep_affex_result["StatusID"]
     result_unpacked["StudyID"] = deep_affex_result["StudyID"]
-    
+    # print("==================================")
+    # print(deep_affex_result.keys())
+    # print("==================================")
     deep_affex_measures = deep_affex_result["Results"]
     
     # Important value HB_BPM, SNR
