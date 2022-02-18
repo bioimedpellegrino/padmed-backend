01a953840a43aae908d73126b571bb02027de704
Ale Pelle <alessandro.pellegrino9494@gmail.com>
2022-02-18 21:17:08 +0100

    dev: development,

--- deepaffex/api.py
+++ deepaffex/api.py
@@ -231,7 +231,16 @@ async def make_measure(config, config_path, video_path, demographics=None, start
         if demographics is not None:
             with open(demographics, "r") as f:
                 app.demographics = json.load(f)
-
+        if not demographics:
+            app.demographics = {
+                "gender": "male",
+                "age": 32,
+                "height": 180,
+                "weight": 70,
+                "smoking": 0,
+                "diabetes": 0,
+                "bloodpressuremedication": 0
+            }
         # Create a face tracker
         if settings.FACE_TRACKER == 'VISAGE':
             tracker = VisageTracker(settings.VISAGE_LICENSE,
