38f78c78a37806341fb4d23eb0f7964e694ce066
Ale Pelle <alessandro.pellegrino9494@gmail.com>
2022-02-19 18:23:39 +0100

    fix

--- triage/utils.py
+++ triage/utils.py
@@ -100,7 +100,10 @@ def print_command_measure(measure, date):
     Args:
         measure (_type_): _description_
     """
-    signals_list = measure_json_to_list(measure["measure"])
+    try:
+        signals_list = measure_json_to_list(measure["measure"])
+    except:
+        return ""
     print_command = "<BIG><BOLD><CENTER> PADMED <BR>\n" + "<CENTER>Esito misurazione<BR>\n" + "<CENTER>Data:" + date +"<BR>\n" + "<BOLD>Misurazione:" +"<BR>\n" + "<BOLD>Misurazione:"+ "<BR>\n";
     for signal_list in signals_list:
         command = "<LEFT><BOLD>" + signal_list.name + ":" + " " + signal_list.value + " " + signal_list.unit + "<BR>\n"
