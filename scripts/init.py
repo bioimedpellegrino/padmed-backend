a324c43148fbc6661a3b1d46e7e2a25f5be55bce
Ale Pelle <alessandro.pellegrino9494@gmail.com>
2022-04-16 17:27:08 +0200

    dev: scripts

--- scripts/init.py
+++ scripts/init.py
@@ -7,10 +7,26 @@ def init_triage():
         triage_code, _ = TriageCode.objects.get_or_create(code=triage_code)
 
 def init_reasons(hospital):
-    reasons = ['INCIDENTE','MALESSERE','ALTRO','CONTUSIONE','FERITA','DOLORE ADDOMINALE','DOLORE GENERALE','INFORTUNIO SUL LAVORO']
-    for reason in reasons:
+    reasons = [
+                ("VALUTAZIONE GENERALE", 1, "WHITE"),
+                ("INFORTUNIO SUL LAVORO", 2, "YELLOW"),
+                ("MALESSERE", 3, "GREEN"),
+                ("CONTUSIONE",4, "YELLOW"),
+                ("DIFFICOLTA' A RESPIRARE", 5, "YELLOW"),
+                ("FERITA", 6, "YELLOW"),
+                ("DOLORE ADDOMINALE", 7, "YELLOW"),
+                ("ALTRO", 8, "WHITE")
+            ]
+    
+    for reason_pack in reasons:
         r = TriageAccessReason()
         r.hospital = hospital
-        r.reason = reason
+        r.reason = reason_pack[0]
+        r.order = reason_pack[1]
+        r.related_code = TriageCode.objects.get(code=reason_pack[2])
         r.save()
     
+    
+
+
+
