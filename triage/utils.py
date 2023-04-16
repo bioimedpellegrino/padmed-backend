97f65a1d40f11bac8221b0babbdecb4a9c8c0c54
Alessandro Pellegrino <alessandro.pellegrino9494@gmail.com>
2023-04-16 13:13:58 +0200

    .

--- triage/utils.py
+++ triage/utils.py
@@ -58,7 +58,7 @@ def generate_video_measure(file_path, video_id, video_settings=None):
         add_log(level=5, message=1, exception=traceback.format_exc(), custom_message=message)
         frame_rate = 24
     # SAVE TO VIDEO FILE, WITH METADATA
-    output_frames = frame_rate * 35
+    output_frames = frame_rate * 30
     if output_frames <= len(video_frames):
         video_frames = video_frames[:output_frames]
         
